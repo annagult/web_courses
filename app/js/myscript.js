064804156a93c9fd5b46e4a518c4846be278ad30
Anna Gultyayeva <126551079+annagult@users.noreply.github.com>
2023-03-28 11:31:49 +0500

    Add files via upload
    То что делали на паре по основам создания сайтов

---
+++ app/js/myscript.js
@@ -0,0 +1,16 @@
+document.write("<h2>Второй</h2>");
+document.write("15 + 25 =");
+var summary = 15+25;
+document.write(summary);
+
+let age = prompt("How old are you?", 100);
+alert("You're old")
+let Boss = confirm("The first one here");
+alert(Boss);
+
+function MyFunction(){
+    document.getElementById("demo").innerHTML=" I'm using JavaScript";
+}
+function FunctionMy(){
+    document.getElementById("demoOne").innerHTML=" I'm learning JavaScript";
+}
